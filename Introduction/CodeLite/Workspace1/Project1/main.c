1baca76c03922739517d76f627aeb11596365c9c
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-06 15:19:55 +0900

    test

--- Introduction/CodeLite/Workspace1/Project1/main.c
+++ Introduction/CodeLite/Workspace1/Project1/main.c
@@ -2,6 +2,22 @@
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+    int count = 0;
+    char ch;
+    char pch=' ';
+    while(1)  {
+        ch=getchar();
+        
+        if(ch== '\n' || ch==EOF)
+        {   
+            break;
+        }
+        else if(pch==' ' && ch!=' ')
+             count++;
+             
+        pch=ch;
+    }
+    printf("%d\n", count);
+    
+    return 0;
 }
