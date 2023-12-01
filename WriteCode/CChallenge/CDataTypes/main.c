7516c6b1189a00a42aff1ede3341bdd961449a89
GeonwooVincentKim <kdsnop@gmail.com>
2023-12-01 16:12:26 +0900

    [Feature] - _Bool and bool

---
+++ WriteCode/CChallenge/CDataTypes/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//#include <stdbool.h> // C99 Standard
+
+int main(int argc, char **argv)
+{
+//    _Bool myBoolean = 1; // C89 Standard
+    bool myBoolean1 = true; // You have to use stdbool header if 
+	printf("hello world\n");
+	return 0;
+}
