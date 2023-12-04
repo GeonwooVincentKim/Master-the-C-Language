23588efdfcfa9fe88d3a70bcabd9ba74ca6f5aab
GeonwooVincentKim <kdsnop@gmail.com>
2023-12-04 17:05:21 +0900

    test

--- WriteCode/CChallenge/CDataTypes/main.c
+++ WriteCode/CChallenge/CDataTypes/main.c
@@ -6,5 +6,6 @@ int main(int argc, char **argv)
     _Bool myBoolean = 1; // C89 Standard
     bool myBoolean1 = true; // You have to use stdbool header if 
 	printf("hello world\n");
+    
 	return 0;
 }
