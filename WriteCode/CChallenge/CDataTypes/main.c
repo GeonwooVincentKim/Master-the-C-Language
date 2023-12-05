9e68f7560fc0827f48799beaa62180ec7968bc58
GeonwooVincentKim <kdsnop@gmail.com>
2023-12-05 17:09:08 +0900

    [Update] - Enum example

--- WriteCode/CChallenge/CDataTypes/main.c
+++ WriteCode/CChallenge/CDataTypes/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-//#include <stdbool.h> // C99 Standard
+#include <stdbool.h> // C99 Standard
 
 int main(int argc, char **argv)
 {
-    _Bool myBoolean = 1; // C89 Standard
-    bool myBoolean1 = true; // You have to use stdbool header
+//    _Bool myBoolean = 1; // C89 Standard
+//    bool myBoolean1 = true; // You have to use stdbool header
 	printf("hello world\n");
+    
+    enum gender {male, female};
+    enum gender myGender;
+    myGender = male;
+    
+    printf("%d", myGender);
+    
+    enum gender anotherGender = female;
+    bool isMale = (myGender == anotherGender);
+    
+    printf("%d", isMale);
     
 	return 0;
 }
