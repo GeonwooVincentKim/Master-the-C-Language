50d30a74b1d0d10cb331ac91295a1568aa58347c
GeonwooVincentKim <kdsnop@gmail.com>
2023-12-15 15:45:52 +0900

    [Feature] - binary number

--- WriteCode/CChallenge/COperators/main.c
+++ WriteCode/CChallenge/COperators/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int f (int n) {
+    int value = n;
+    
+    if (value > 1) f(value / 2);
+    return value;
+}
+
 int main() {
-    return 0;
+    unsigned int a = 60;
+    unsigned int b = 13;
+    
+    signed int a1 = 60;
+    signed int b1 = 13;
+    
+    int a2 = 60;
+    int b2 = 13;
+    
+    printf("A: ", f(a % 2), f(a1 % 2), f(a2 % 2));
+    printf("B: %d %d %d\n", b, b1, b2);
 }
 
