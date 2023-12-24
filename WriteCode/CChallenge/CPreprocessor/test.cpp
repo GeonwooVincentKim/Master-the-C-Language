0d5beeee2a9cdbc5c97fef22c181605dbb2e46e1
GeonwooVincentKim <kdsnop@gmail.com>
2023-12-24 21:42:16 +0900

    Test

--- WriteCode/CChallenge/CPreprocessor/test.cpp
+++ WriteCode/CChallenge/CPreprocessor/test.cpp
@@ -1,7 +1,7 @@
-///*
-// * Author: Jason Fedin
-// * Purpose: Learn what does pre-processor means
-// * */
+/*
+ * Author: Jason Fedin
+ * Purpose: Learn what does pre-processor means
+ * */
 // 
 // #include <stdio.h>
 // 
